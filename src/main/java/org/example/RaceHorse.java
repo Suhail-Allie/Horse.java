6775fe36d54be519e170a64cfd2b8661fc91ebdb
Suhail-Allie <162137367+Suhail-Allie@users.noreply.github.com>
2024-09-17 22:39:28 +0000

    First Commit

---
+++ src/main/java/org/example/RaceHorse.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+public class RaceHorse extends Horse {
+
+    private int numberOfRaces;
+
+    public RaceHorse(String name, String color, int birthYear, int numberOfRaces) {
+        super(name, color, birthYear);
+        this.numberOfRaces = numberOfRaces;
+    }
+
+    public int getNumberOfRaces() {
+        return numberOfRaces;
+    }
+
+    public void setNumberOfRaces(int numberOfRaces) {
+        this.numberOfRaces = numberOfRaces;
+    }
+}
+
+
+
